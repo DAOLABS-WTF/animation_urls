dc55292a6244db72c6a07f706f4cf8ba07bf861c
EVMCompatible <evmcompatible@gmail.com>
2023-07-11 17:58:33 +0500

    fix 404 error solution of music and p5js as well

--- scripts/ipfs-fallback.js
+++ scripts/ipfs-fallback.js
@@ -4,6 +4,8 @@ async function main() {
 	try {
 		fs.copyFileSync('build/200.html', 'build/ipfs-404.html');
 		fs.copyFileSync('build/video.html', 'build/video');
+		fs.copyFileSync('build/music.html', 'build/music');
+		fs.copyFileSync('build/p5js.html', 'build/p5js');
 	} catch (e) {
 		console.error(e.message || e);
 	}
