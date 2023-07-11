4106a0f719543f03bb458da597569bcee5e6f7ec
EVMCompatible <evmcompatible@gmail.com>
2023-07-11 17:51:26 +0500

    test ipfs 404 solution

--- scripts/ipfs-fallback.js
+++ scripts/ipfs-fallback.js
@@ -3,6 +3,7 @@ import fs from 'fs';
 async function main() {
 	try {
 		fs.copyFileSync('build/200.html', 'build/ipfs-404.html');
+		fs.copyFileSync('build/video.html', 'build/video');
 	} catch (e) {
 		console.error(e.message || e);
 	}
